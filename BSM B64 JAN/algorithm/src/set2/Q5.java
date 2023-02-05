167e3fccee6b6511bda6effbe70cc75871ae1bd6
Haicheng Liu <liu.haic@northeastern.edu>
2023-02-05 18:05:23 -0500

    q5

--- BSM B64 JAN/algorithm/src/set2/Q5.java
+++ BSM B64 JAN/algorithm/src/set2/Q5.java
@@ -1,5 +1,30 @@
 package set2;
 
 public class Q5 {
+	
+	public static void main(String[] args) {
+		
+	}
 
+
+
+
+	public static int[][] multiplyMatrix(int[][] firstMatrix, int[][] secondMatrix) {
+	
+	  int rowsInFirstMatrix = firstMatrix.length;
+	  int columnsInFirstMatrix = firstMatrix[0].length;
+	  int columnsInSecondMatrix = secondMatrix[0].length;
+
+	  int[][] result = new int[rowsInFirstMatrix][columnsInSecondMatrix];
+
+	  for (int i = 0; i < rowsInFirstMatrix; i++) {
+	    for (int j = 0; j < columnsInSecondMatrix; j++) {
+	      for (int k = 0; k < columnsInFirstMatrix; k++) {
+	        result[i][j] += firstMatrix[i][k] * secondMatrix[k][j];
+	      }
+	    }
+	  }
+
+	  return result;
+	}
 }
